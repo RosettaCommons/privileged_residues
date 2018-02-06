abdfbef30ec922cc85785da3c5bd7accb5f873f5
Brian D. Weitzner <brian.weitzner@gmail.com>
2018-02-06 09:10:44 -0800

    Changing around the __init__ imports

--- privileged_residues/__init__.py
+++ privileged_residues/__init__.py
@@ -6,7 +6,4 @@ __author__ = """Brian D. Weitzner"""
 __email__ = 'weitzner@uw.edu'
 __version__ = '0.1.0'
 
-from .hbond_ray_pairs import *
-from .position_residue import *
-from .process_networks import *
-
+from . import hbond_ray_pairs, position_residue, process_networks
