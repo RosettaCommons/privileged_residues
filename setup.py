1ca6a4795704e47b34237817c5dca3e579fef394
Brian D. Weitzner <brian.weitzner@gmail.com>
2018-02-13 17:21:30 -0800

    Bump version: 0.1.0 → 0.2.0

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ test_requirements = [
 
 setup(
     name='privileged_residues',
-    version='0.1.0',
+    version='0.2.0',
     description="Privileged Residues contains methods for placing residues on the surface of a target protein that can be added to a RIF.",
     long_description=readme + '\n\n' + history,
     author="Brian D. Weitzner",
