c2457c3a5142a2b3db9a45a8f1bfb5c5edc7fc78
Brian D. Weitzner <brian.weitzner@gmail.com>
2018-03-07 15:11:29 -0800

    Adding support for processing a dataset of bidentate hydrogen bonds

--- privileged_residues/__init__.py
+++ privileged_residues/__init__.py
@@ -2,7 +2,7 @@
 
 """Top-level package for Privileged Residues."""
 from .privileged_residues import *
-from . import hbond_ray_pairs, position_residue, process_networks
+from . import hbond_ray_pairs, position_residue, process_bidentates, process_networks
 
 __author__ = """Brian D. Weitzner"""
 __email__ = 'weitzner@uw.edu'
