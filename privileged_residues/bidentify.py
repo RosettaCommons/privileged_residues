1e03613934f9341fa6d39f4fa3a0de4cc44b7c89
Brian D. Weitzner <brian.weitzner@gmail.com>
2018-03-16 18:04:11 -0700

    Adding an initial skeleton for the residue placer

---
+++ privileged_residues/bidentify.py
@@ -0,0 +1,29 @@
+import os
+
+# the object of this submodule is to scan over all surface residues and 
+# identify pairs of rays to look up in each hash table
+
+def 
+def look_up_bidentate_hbond():
+	pass
+
+
+def look_up_connected_network():
+	pass 
+
+
+def find_hashable_positions():
+	# look up each type of privileged interaction
+	#
+	# networks:
+	#     iterate over pairs of residues with side chains that are 
+	#     already hydrogen bonded to one another
+	# Sc_Sc bidentates:
+	#     iteratate over every residue, throw out non-bidentate capable 
+	#     residues
+	# Sc_ScBb bidentates:
+	#     iterate over pairs of neighboring residues on the surface
+	# Sc_Bb bidentates:
+	#     iterate over pairs of neighboring residues on the surface
+	
+
