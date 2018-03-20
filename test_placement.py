ebfa37a48257577ae0914c599706418c81b8f726
Brian D. Weitzner <brian.weitzner@gmail.com>
2018-03-19 17:43:01 -0700

    Adding the script to get everything set up in an interactive shell to keep testing

---
+++ test_placement.py
@@ -0,0 +1,11 @@
+import pyrosetta
+import privileged_residues
+from privileged_residues.privileged_residues import _init_pyrosetta as init
+import numpy as np
+from privileged_residues import bidentify as bd
+
+init()
+
+cart_resl, ori_resl, cart_bound = 0.1, 2.0, 16.0
+p = pyrosetta.pose_from_file('sc_bb_example.pdb')
+pairs, ht = privileged_residues.find_privileged_interactions_in_pose(p)
