a04b05e86f9597fc5d5ebc900556592f666c9ebf
Brian D. Weitzner <brian.weitzner@gmail.com>
2018-03-22 16:16:47 -0700

    Updating the placement test script. Once this is working, it should be turned into a pytest

--- test_placement.py
+++ test_placement.py
@@ -9,3 +9,9 @@ init()
 cart_resl, ori_resl, cart_bound = 0.1, 2.0, 16.0
 p = pyrosetta.pose_from_file('sc_bb_example.pdb')
 pairs, ht = privileged_residues.find_privileged_interactions_in_pose(p)
+
+hits = bd.look_up_interactions(pairs, ht, cart_resl, ori_resl, cart_bound)
+
+for i, hit in enumerate(hits):
+    hit.dump_pdb('result_{:04d}.pdb'.format(i + 1))
+    
