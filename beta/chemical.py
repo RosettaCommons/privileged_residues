8de0751fe5159ec35d373696787342c883e012a8
Tarik Onalan <onalant@users.noreply.github.com>
2018-05-04 11:44:31 -0700

    Still thinking about how to structure table loading.
    Otherwise, continuing to bring in isomorphic functionality from
    old interface (Ex: pose transformations and chemical information)

---
+++ beta/chemical.py
@@ -0,0 +1,45 @@
+from collections import namedtuple
+
+# the order of the keys of this dictionary
+FunctionalGroup = namedtuple('FunctionalGroup', ['resName', 'donor', 'acceptor', 'atoms'])
+FunctionalGroup.__doc__ = """Store hydrogen bonding information about a
+functional group as well as information that can be used to position it
+in three-space.
+
+Attributes:
+    resName (str): Name of the functional group.
+    donor (bool): True if the functional group can be a donor in a
+        hydrogen bond.
+    acceptor (bool): True if the functional group can be an acceptor in
+        a hydrogen bond.
+    atoms (list): A list of three atom names (str) that are used to
+        construct a coordinate frame to describe the position of the
+        functional group in space.
+"""
+
+functional_groups = {
+	"OH_": FunctionalGroup("hydroxide", True, True, ["CV", "OH", "HH"]),
+	"G__": FunctionalGroup("guanidinium", True, False, ["CZ", "NH1", "NH2"]),
+	"I__": FunctionalGroup("imidazole", True, True, ["ND1", "CD2", "NE2"]),
+	# imidazole tautomer
+	"ID_": FunctionalGroup("imidazole_D", True, True, ["ND1", "CD2", "NE2"]),
+	"A__": FunctionalGroup("amine", True, False, ["NZ", "1HZ", "2HZ"]),
+	"C__": FunctionalGroup("carboxylate", False, True, ["CD", "OE1", "OE2"]),
+	"CA_": FunctionalGroup("carboxamide", True, True, ["CG", "OD1", "ND2"])
+}
+
+ResInfo = namedtuple('ResName', ['grp', 'atoms'])
+rsd_to_fxnl_grp = {
+	"ASN": ResInfo("CA_", ["CG", "OD1", "ND2"]),
+	"GLN": ResInfo("CA_", ["CD", "OE1", "NE2"]),
+	"ASP": ResInfo("C__", ["CG", "OD1", "OD2"]),
+	"GLU": ResInfo("C__", ["CD", "OE1", "OE2"]),
+	# Use HD1/HE2 to determine which tautomer to use
+	"HIS": ResInfo("I__", ["ND1", "CD2", "NE2"]),
+	"ARG": ResInfo("G__", ["CZ", "NH1", "NH2"]),
+	"SER": ResInfo("OH_", ["CB", "OG", "HG"]),
+	"THR": ResInfo("OH_", ["CB", "OG1", "HG1"]),
+	"TYR": ResInfo("OH_", ["CZ", "OH", "HH"]),
+	"LYS": ResInfo("A__", ["NZ", "1HZ", "2HZ"])
+}
+
--- beta/table.py
+++ beta/table.py
@@ -1,19 +1,35 @@
 import numpy as np
+import rif
 
 from collections import namedtuple
-from rif.hash import XformHash_bt24_BCC6_X3f
-from typing import Iterator, MappingView
 
-class ResidueTable(Mapping[np.uint64, pyrosetta.Pose]):
+from rif.geom import rif.geom.Ray
+from rif.geom.ray_hash import RayToRay4dHash
+from rif.hash import XformHash_bt24_BCC6_X3f
+
+from typing import Iterable, MappingView
+
+import .transform
+
+# NOTE(onalant): <C-g> will show current file in nvi!
+
+_LEVER = 10
+_BOUND = 1000
+
+class ResidueTable(Mapping[np.uint64, Iterable[pyrosetta.Pose]]):
 
 	def __init__(self, cart_resl: float = 0.1, ori_resl: float = 2.0, cart_bound: float = 16.0) -> None:
+		self._raygrid = RayToRay4dHash(ori_resl, _LEVER, bound=_BOUND)
 		self._lattice = XformHash_bt24_BCC6_X3f(cart_resl, ori_resl, cart_bound)
 		self._table = None
 
-	def __index__(self, key: np.uint64) -> Iterator[pyrosetta.Pose]:
-		transform = xh.get_center([bin])['raw'].squeeze()
+	def __get_item__(self, key: np.uint64) -> Iterable[pyrosetta.Pose]:
+		transform = self._lattice.get_center([bin])['raw'].squeeze()
 		results = self.table[key]
 
-	def _transform_pose(self, pose: pyrosetta.Pose, xform: np.array):
+	def __iter__(self):
 		pass
+
+	def __len__(self):
+		return len(self.table)
 
