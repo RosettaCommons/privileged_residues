adc8c9660f2ae7389620b1fb5b80e1c0e10e301b
Brian D. Weitzner <brian.weitzner@gmail.com>
2018-06-15 18:31:50 -0700

    Updating basic matcher to support my workflow

--- basic_matcher.py
+++ basic_matcher.py
@@ -69,11 +69,17 @@ def make_parser():
                              "Allowed values are: 'acceptor_acceptor', "
                              "'acceptor_donor', 'donor_acceptor', " +
                              "'donor_donor'.")
+
+    feature_parser = parser.add_mutually_exclusive_group(required=False)
+    feature_parser.add_argument('--reduced-output', dest='reduced_output', action='store_true')
+    feature_parser.add_argument('--no-reduced-output', dest='reduced_output', action='store_false')
+    parser.set_defaults(reduced_output=False)
+
     return parser
 
 
 def get_hits_for_interaction_type(ht_name_full, pdb_file, residues=None,
-                                  outpath=None):
+                                  outpath=None, reduced_output=False):
 
     p = pyrosetta.pose_from_pdb(path.expanduser(pdb_file))
     selector = TrueResidueSelector()
@@ -93,22 +99,86 @@ def get_hits_for_interaction_type(ht_name_full, pdb_file, residues=None,
         out = path.expanduser(outpath)
         makedirs(out, exist_ok=True)
         p.dump_pdb(path.join(out, "ori.pdb"))
+    
 
-    n = 0
-    i_n = 0
-    old_in = 0
-    for interaction_number, hit in bidentify.look_up_interactions(pairs_of_rays,
-                                 ht, ht_info.cart_resl, ht_info.ori_resl,
-                                 ht_info.cart_bound):
-        if interaction_number != old_in:
-            old_in = interaction_number
-            i_n += 1
+    if reduced_output:
+        hits_and_in = list(bidentify.look_up_interactions(pairs_of_rays,
+                                     ht, ht_info.cart_resl, ht_info.ori_resl,
+                                     ht_info.cart_bound))
+        if not len(hits_and_in):
+            return
+
+        last_in = hits_and_in[-1][0]
+        hits = [None for _ in range(last_in + 1)]
+
+        for interaction_number, hit in hits_and_in:
+            if hits[interaction_number] is None:
+                hits[interaction_number] = []
+            hits[interaction_number].append(hit)
+
+        import pyrosetta.rosetta.core.scoring as scoring
+        clash_cutoff = 100.
+        sfx = pyrosetta.rosetta.core.scoring.ScoreFunctionFactory.create_score_function("beta_nov16")
+        sfx.set_weight(scoring.hbond_bb_sc, 2.0)
+        sfx.set_weight(scoring.hbond_sc, 2.0)
+
+        mmap = pyrosetta.rosetta.core.kinematics.MoveMap()
+
+        mmap.set_bb(False)
+        mmap.set_chi(False)
+        mmap.set_jump(1, True)
+
+        minmov = pyrosetta.rosetta.protocols.minimization_packing.MinMover(
+            mmap, sfx, "dfpmin_armijo_nonmonotone", 0.01, False)
+
+        for interaction_number, hit_grp in enumerate(hits):
+            best_score = 666. # the number of the beast!
+            best_hit = None
+            if hit_grp is None:
+                continue
             n = 0
-        n += 1
-            
-        hit.dump_pdb(path.join(out, '{}_{}_result_{:04d}.pdb'.format(
-            i_n, ht_info.type, n)))
-    print('There are {} hits for {} interactions.'.format(n, ht_info.type))
+            for hit in hit_grp:
+                proto_pose = p.clone()
+                proto_pose.append_pose_by_jump(hit, len(proto_pose.residues))
+
+                sfx(proto_pose)
+
+                fa_rep = proto_pose.energies().total_energies()[scoring.fa_rep]
+
+                # unclear if this necessary, but I could imagine large clashes encouraging 
+                # the minimizer to move the fxnl grp far away, which could end up artiifcially
+                # improving the score.
+                if fa_rep > clash_cutoff:
+                    continue
+
+                n += 1
+
+                minmov.apply(proto_pose)
+                curr_score = sfx(proto_pose)
+                if best_score > curr_score:
+                    best_score = curr_score
+                    best_hit = hit
+
+            if best_hit is not None:
+                best_hit.dump_pdb(path.join(out, '{}_{}_result_{:04d}.pdb'.format(
+                    ht_info.type, interaction_number, n)))
+
+    else:
+        n = 0
+        i_n = 0
+        old_in = 0
+        for interaction_number, hit in bidentify.look_up_interactions(pairs_of_rays,
+                                     ht, ht_info.cart_resl, ht_info.ori_resl,
+                                     ht_info.cart_bound):
+            if interaction_number != old_in:
+                old_in = interaction_number
+                i_n += 1
+                n = 0
+            n += 1
+                
+            hit.dump_pdb(path.join(out, '{}_{}_result_{:04d}.pdb'.format(
+                i_n, ht_info.type, n)))
+        print('There are {} hits for {} interactions.'.format(n, ht_info.type))
 
 
 def get_hits_from_tables(hash_tables, p, selector):
@@ -132,7 +202,7 @@ def main(argv):
                              'Valid options are: "{}"'.format('", "'.join(
                                 _bidentate_types)))
     networks = args.networks
-    print(networks)
+    # print(networks)
     for n in args.networks:
         try:
             if not json.loads(n.lower()):
@@ -180,7 +250,7 @@ def main(argv):
             selector.append_index(idx)
     '''
 
-    arguments = [(ht, args.PDBFile, args.residues, args.outpath) for ht in
+    arguments = [(ht, args.PDBFile, args.residues, args.outpath, args.reduced_output) for ht in
                  hash_tables]
     with Pool(processes=cpu_count()) as pool:
         pool.starmap(get_hits_for_interaction_type, arguments)
