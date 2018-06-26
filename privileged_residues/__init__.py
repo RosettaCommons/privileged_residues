1b979caec1111eae50249796b0a2dae06e377929
Tarik Onalan <onalant@users.noreply.github.com>
2018-06-26 11:43:12 -0700

    Shuffling files around

--- privileged_residues/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-"""Top-level package for Privileged Residues."""
-from .privileged_residues import *
-from . import bidentify, hbond_ray_pairs, position_residue, process_bidentates, process_networks
-
-__author__ = """Brian D. Weitzner"""
-__email__ = 'weitzner@uw.edu'
-__version__ = '0.2.0'
--- privileged_residues/privileged_residues.py
+++ privileged_residues/privileged_residues.py
@@ -1,273 +1,146 @@
-# -*- coding: utf-8 -*-
-"""Main module."""
 import logging
+import numpy as np
+import pyrosetta
+import table
 
-# The following packages are not pip-installable
-# The import calls are wrapped in a try/except block
-try:
-    import pyrosetta
-    HAVE_PYROSETTA = True
-except ImportError:
-    print('Module "pyrosetta" not found in the current environment! '
-          'Go to http://www.pyrosetta.org to download it.')
-    HAVE_PYROSETTA = False
-    pass
+import chemical
+import geometry
+import util
 
-from os import path
-_dir = path.join(path.dirname(__file__), 'data', 'functional_groups')
+from rif.geom import Ray
+from rif.geom.ray_hash import RayToRay4dHash
+from rif.hash import XformHash_bt24_BCC6_X3f
+
+from pyrosetta.rosetta.core.conformation import ResidueFactory
+from pyrosetta.rosetta.core import scoring
 
 logging.basicConfig(level=logging.WARN)
-_logging_handler = 'interactive'
+_logging_handler = "interactive"
 
-_PYROSETTA_INIT = False
-
+# WARN(onalant): Constants recommended by Will Sheffler; do not change unless /absolutely/ sure
+LEVER = 10
+BOUND = 1000
+MODE = "fa_standard"
 
 def _init_pyrosetta():
-    # initialize pyrosetta
-    assert(HAVE_PYROSETTA)
-    global _PYROSETTA_INIT
-    if _PYROSETTA_INIT:
-        return
+    from os import path
 
-    from . import process_networks as pn
-    params_files_list = [path.join(_dir, t.resName) + '.params' for _, t in
-                         pn.fxnl_groups.items()]
-    opts = ['-corrections:beta_nov16', '-ignore_waters false', '-mute core',
-            '-extra_res_fa {}'.format(' '.join(params_files_list)),
-            '-constant_seed', '-output_virtual']
-    pyrosetta.init(extra_options=' '.join(opts),
-                   set_logging_handler=_logging_handler)
-    _PYROSETTA_INIT = True
+    _dir = path.join(path.dirname(__file__), "data", "functional_groups")
 
+    param_files = [path.join(_dir, x.resName + ".params") for x in chemical.functional_groups.values()]
+    opts = [
+        "-corrections:beta_nov16",
+        "-ignore_waters false",
+        "-mute core",
+        "-extra_res_fa %s" % (" ".join(param_files)),
+        # "-constant_seed",
+        "-output_virtual"
+    ]
 
-def hash_ray_pairs_from_pdb_file(argv):
-    """
-    """
-    from . import hbond_ray_pairs as hrp
+    pyrosetta.init(extra_options=" ".join(opts), set_logging_handler=_logging_handler)
 
-    opts = ['-add_orbitals']
-    target_fname = '../DHR10_133144_1.pdb'  # test!
+class PrivilegedResidues:
 
-    pyrosetta.init(extra_options=' '.join(opts),
-                   set_logging_handler=_logging_handler)
-    p = pyrosetta.pose_from_file(target_fname)
-    hbset = hrp.find_hbonds(p)
-    surf_hbset = hrp.identify_bonded_pairs(p, hbset)
+    def __init__(self, path = "/home/onalant/dump/2018-05-07_datatables/database.h5"):
+        self._data = table.GenericTable(path)
 
-    # make a list of the rays and then concatenate them with np.stack()
-    donors = []
-    acceptors = []
-    for hbond in surf_hbset.hbonds():
-        don_ray, acc_ray = hrp.find_ray_pair_for_hbond(p, hbond)
-        donors.append(don_ray)
-        acceptors.append(acc_ray)
+        cart_resl = self._data._table.attrs["cart_resl"]
+        ori_resl = self._data._table.attrs["ori_resl"]
+        cart_bound = self._data._table.attrs["cart_bound"]
 
-    hrp.hash_rays(np.stack(donors), np.stack(acceptors))
+        self._lattice = XformHash_bt24_BCC6_X3f(cart_resl, ori_resl, cart_bound)
+        self._raygrid = RayToRay4dHash(ori_resl, LEVER, bound=BOUND)
 
+    # bidentate: "sc_sc", "sc_scbb", "sc_bb"
+    # network: "acceptor_acceptor", "acceptor_donor", "donor_acceptor", "donor_donor"
+    def match(self, ray1, ray2, group):
+        dummy_pose = pyrosetta.pose_from_sequence("A", "fa_standard")
+        res_type_set = dummy_pose.conformation().residue_type_set_for_conf()
 
+        def _numpy_to_rif_ray(r):
+            return r.astype('f4').reshape(r.shape[:-2] + (8,)).view(Ray)
 
-def hash_all_network_files_in_directory(root_dir, out_dir='hash_tables',
-                                        fname='functional_stubs.pdb',
-                                        cart_resl=.1, ori_resl=2.,
-                                        cart_bound=16.):
-    """
+        hashed_rays = np.asscalar(self._raygrid.get_keys(*(_numpy_to_rif_ray(r) for r in [ray1, ray2])).squeeze())
 
-    Args:
-        fname (str):
-        out_dir (str):
-        fname (str): A pattern for network files in the subdirectories
-            of root_dir.
-        cart_resl (float): The cartesian resolution of the BCC lattice.
-            Defaults to 0.1.
-        ori_resl (float): The orientational resolution of the BCC
-            lattice. In general, ori_resl should be roughly
-            20 * cart_resl. Defaults to 2.0.
-        cart_bound (float): The bounds of the lattice. Defaults to 16.
-
-    """
-    from os import path, makedirs, walk
-
-    _init_pyrosetta()
-
-    # get a list of the files that need to be processed
-    arrangement_files = []
-    output_file_base_names = []
-    for directory, sub_dirs, files in walk(path.abspath(root_dir)):
-        if fname in files:
-            _, arr = path.split(directory)
-            arrangement_files.append(path.join(directory, fname))
-            output_file_base_names.append(path.join(out_dir),
-                path.basename(path.normpath(directory)))
-
-    if not path.exists(out_dir):
-        makedirs(out_dir)
-
-    # iterate over all of the files and make a giant ndarry containing all
-    # of the information. This thing is going to take up some serious memory.
-    for ntwrk_file in arrangement_files:
-        hash_networks_and_write_to_file(fname, out_dir, cart_resl, ori_resl,
-                                        cart_bound)
-
-def _hash_from_file(fname, out_name_base, cart_resl, ori_resl, cart_bound, mod):
-    import numpy as np
-    import pickle
-    from . import process_networks as pn
-
-    _init_pyrosetta()
-
-    hash_types = []
-    print(fname)
-    for i, pose in enumerate(pn.poses_for_all_models(fname)):
-        if not i % 100:
-            print('Pose ' + str(i))
+        results = self._data[hashed_rays, group]
         try:
-            hash_types.extend(mod.find_all_relevant_hbonds_for_pose(pose))
-        except AssertionError:
-            print('Model {} fails!'.format(i))
-            pose.dump_pdb('fail_mdl.pdb')
-            import sys
-            sys.exit()
+            ray_frame = geometry.rays_to_transform(ray1, ray2)
+        except:
+            return []
 
-    if hash_types == []:
-        return
-    # hash all of the processed infromation
-    ht = pn.hash_full(np.stack(hash_types), cart_resl, ori_resl, cart_bound)
+        for pos_info in results:
+            try:
+                stored_frame = self._lattice.get_center([pos_info["transform"]])['raw'].squeeze()
+            except:
+                continue
 
-    # split the table into smaller tables based on interaction types
-    # and write to disk as a pickled dictionary. Keys are the hashed ray pairs,
-    # values are a list of (id, bin)s
-    for i, interaction_type in enumerate(mod.interaction_types):
-        t = ht[np.isin(ht['it'], [i])]
-        out_fname = out_name_base + '_' + '_'.join([interaction_type,
-                                                    str(cart_resl),
-                                                    str(ori_resl),
-                                                    str(cart_bound)]) + '.pkl'
-    
-        with open(out_fname, 'wb') as f:
-            fdata = {}
-            for k, v in zip(t['key'], t[['id', 'hashed_ht']]):
-                try:
-                    fdata[k].add(tuple(v))
-                except KeyError:
-                    fdata[k] = {tuple(v)}
-            pickle.dump(fdata, f)
+            resname = pos_info["residue"].decode("utf-8")
+            pos_grp = chemical.functional_groups[resname]
 
+            dummy_pose.replace_residue(1, ResidueFactory.create_residue(res_type_set.name_map(resname)), False)
 
-def hash_networks_and_write_to_file(fname, out_name_base, cart_resl=.1,
-                                    ori_resl=2., cart_bound=16.):
-    """
+            coords = [np.array([*dummy_pose.residues[1].xyz(atom)]) for atom in pos_grp.atoms]
+            c = np.stack(coords)
 
-    Args:
-        fname (str):
-        out_dir (str):
-        cart_resl (float): The cartesian resolution of the BCC lattice.
-            Defaults to 0.1.
-        ori_resl (float): The orientational resolution of the BCC
-            lattice. In general, ori_resl should be roughly
-            20 * cart_resl. Defaults to 2.0.
-        cart_bound (float): The bounds of the lattice. Defaults to 16.
-    """
-    from . import process_networks as pn
-    _hash_from_file(fname, out_name_base, cart_resl, ori_resl, cart_bound, pn)
+            pos_frame = geometry.coords_to_transform(c)
+            final = np.dot(np.dot(ray_frame, stored_frame), np.linalg.inv(pos_frame))
+            dummy_pose.apply_transform(final)
+            yield dummy_pose.clone()
 
+	# NOTE(onalant): bring your own residue selector
+    def search(self, pose, groups=["bidentate"], selector):
+        pairs_of_rays = []
 
-def hash_bidentates_and_write_to_file(fname, out_name_base, cart_resl=.1,
-                                      ori_resl=2., cart_bound=16.):
-    """
+        if np.any([x in groups for x in ["sc_sc", "bidentate"]]):
+            pairs_of_rays += look_for_sc_sc_bidentates(p, selector)
+        if np.any([x in groups for x in ["sc_scbb", "bidentate"]]):
+            pairs_of_rays += look_for_sc_scbb_bidentates(p, selector)
+        if np.any([x in groups for x in ["sc_bb", "bidentate"]]):
+            pairs_of_rays += look_for_sc_bb_bidentates(p, selector)
 
-    Args:
-        fname (str):
-        out_dir (str):
-        cart_resl (float): The cartesian resolution of the BCC lattice.
-            Defaults to 0.1.
-        ori_resl (float): The orientational resolution of the BCC
-            lattice. In general, ori_resl should be roughly
-            20 * cart_resl. Defaults to 2.0.
-        cart_bound (float): The bounds of the lattice. Defaults to 16.
-    """
-    from . import process_bidentates as pb
-    _hash_from_file(fname, out_name_base, cart_resl, ori_resl, cart_bound, pb) 
+        if np.any([x in groups for x in ["acceptor_acceptor", "network"]]):
+            pairs_of_rays += look_up_connected_network(p, selector, accA=True, accB=True)
+        if np.any([x in groups for x in ["acceptor_donor", "network"]]):
+            pairs_of_rays += look_up_connected_network(p, selector, accA=True, donB=True)
+        if np.any([x in groups for x in ["donor_acceptor", "network"]]):
+            pairs_of_rays += look_up_connected_network(p, selector, donA=True, accB=True)
+        if np.any([x in groups for x in ["donor_donor", "network"]]):
+            pairs_of_rays += look_up_connected_network(p, selector, donA=True, donB=True)
 
+        for (r1, r2) in pairs_of_rays:
+            yield from self.match(r1, r2, groups)
 
-def laod_hash_tables_from_disk(fname=None):
-    """Load the hash tables (pickled dictionaries) containing hydrogen
-    bond ray pair hashes as keys and a functional group identifier and
-    a bin number of Body Centered  Cubic (BCC) lattice corresponding to
-    a homogenous transform that will place the functional group relative
-    to the hydrogen bonding rays as values. In its current state, this
-    script selects an arbitrary item from the table and ensures that a
-    functional group can be placed correctly.
+    def filter_clash_minimize(p, hits, clash_cutoff=35.0, sfx=None, mmap=None):
+        if (not sfx):
+            sfx = scoring.ScoreFunctionFactory.create_score_function("beta_nov16")
 
-    Args:
-        fname (str):
+            sfx.set_weight(scoring.hbond_bb_sc, 2.0)
+            sfx.set_weight(scoring.hbond_sc, 2.0)
 
-    """
-    import numpy as np
-    import pickle
-    from os import path
-    from . import process_networks
-    from . import position_residue as pr
+        if (not mmap):
+            mmap = pyrosetta.rosetta.core.kinematics.MoveMap()
 
-    _init_pyrosetta()
+            mmap.set_bb(False)
+            mmap.set_chi(False)
+            mmap.set_jump(1, True)
 
-    # for now I'll just focus on a single file
-    hash_tables = {}
-    hash_info = {}
-    fname = '{}_0.1_2.0_16.0.pkl'
-    for t in process_networks.interaction_types:
-        fn = fname.format(t)
-        table_data = pr._fname_to_HTD(fn)
-        hash_info[t] = table_data
-        with open(fn, 'rb') as f:
-            hash_tables[t] = pickle.load(f)
+        minmov = pyrosetta.rosetta.protocols.minimization_packing.MinMover(mmap, sfx, "dfpmin_armijo_nonmonotone", 0.01, False)
 
-    # ok, let's grab something silly to make a good test:
-    table = 'donor_donor'
-    hashed_rays = next(iter(hash_tables[table].keys()))
-    positioning_info = hash_tables[table][hashed_rays]
+        for hit in hits:
+            proto_pose = p.clone()
+            proto_pose.append_pose_by_jump(hit, len(proto_pose.residues))
 
-    # now positioning_info is a set of possible values
-    fxnl_grps = list(process_networks.fxnl_groups.keys())
-    xform = pr.get_ht_from_table(positioning_info[0][1],
-                                 hash_info[table].cart_resl,
-                                 hash_info[table].ori_resl,
-                                 hash_info[table].cart_bound)
+            sfx(proto_pose)
 
-    rsd = fxnl_grps[positioning_info[0]]
-    pos_grp = process_networks.fxnl_groups[rsd]
-    p = pyrosetta.Pose()
-    pyrosetta.make_pose_from_sequence(p, 'Z[{}]'.format(rsd), 'fa_standard')
-    coords = [np.array([*p.residues[1].xyz(atom)]) for atom in pos_grp.atoms]
-    c = np.stack(coords)
-    pos_frame = np.linalg.inv(hbond_ray_pairs.get_frame_for_coords(c))
+            fa_rep = proto_pose.energies().total_energies()[scoring.fa_rep]
+            fa_elec = proto_pose.energies().total_energies()[scoring.fa_elec]
 
-    xf = np.dot(pos_frame, xform)
-    pr.transform_pose(p, xf)
-    # convert pose to ATOM records append all together and write them to a file
+            if (fa_rep + fa_elec > clash_cutoff):
+                continue
 
+            minmov.apply(proto_pose)
 
-def find_privileged_interactions_in_pose(p, ht_name='Sc_Bb_0.1_2.0_16.0.pkl'):
-    """
-    """
-    from os import path
-    import pickle
-    from . import bidentify as bd
-    from . import position_residue as pr
+            yield proto_pose
 
-    # ht_name = 'Sc_Bb_0.1_2.0_16.0.pkl'
-
-    # this is a little hacky, but it makes my life easier for now:
-    # ep -> extra path
-    import sys
-    ep = 'digs/' if sys.platform == 'darwin' else ''
-
-    ht_path = path.expanduser('~weitzner/{}bidentate_hbond_pdbs_2/hash_tables'.format(ep))
-    ht_name_full = path.join(ht_path, ht_name)
-    table_data = pr._fname_to_HTD(ht_name)
-    
-    with open(ht_name_full, 'rb') as f:
-        ht = pickle.load(f)
-    pairs_of_rays = bd.find_hashable_positions(p, ht)
-
-    return pairs_of_rays, ht
+_init_pyrosetta()
 
