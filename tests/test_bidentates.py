df5b2c3212c46a3c28aa024faa93eacfe883bcb2
Tarik Onalan <onalant@users.noreply.github.com>
2018-07-13 19:17:30 -0700

    More fiddling with tests

--- tests/test_bidentates.py
+++ tests/test_bidentates.py
@@ -4,45 +4,38 @@ import pyrosetta
 import numpy as np
 import os
 import random
-import unittest
+import pytest
 
 from os import path
 from privileged_residues import chemical
 from rmsd import rmsd
 
-class Bidentates(unittest.TestCase):
-    def bidentate(self, suffix):
-        pdb_dir = "/home/weitzner/bidentate_hbond_pdbs_2/00_hashHBs_Sc_%s/pdb/" % (suffix)
-        prefix = random.choice(os.listdir(pdb_dir))
-        test_file = random.choice(os.listdir(path.join(pdb_dir, prefix)))
-        test_file = path.join(pdb_dir, prefix, test_file)
-        pose = next(privileged_residues.util.models_from_pdb(test_file))
-        print(test_file)
+curdir = path.dirname(path.abspath(__file__))
+data = [ (path.basename(bdtype), path.splitext(respair)[0]) for (bdtype, _, respairs) in os.walk(path.join(curdir, "data", "bidentates")) for respair in respairs ]
 
-        selector = pyrosetta.rosetta.core.select.residue_selector.ResidueIndexSelector()
-        pres = privileged_residues.PrivilegedResidues()
+def idfn(value):
+    return "%s - %s" % value
 
-        selector.append_index(5)
+@pytest.mark.parametrize("bdtype, respair", data, ids=idfn)
+def test_bidentate(bdtype, respair):
+    pdb = path.join(curdir, "data", "bidentates", bdtype, "%s.pdb" % (respair))
+    pose = next(privileged_residues.util.models_from_pdb(pdb))
 
-        ref_res = pose.residue(2)
-        ref_coords = np.stack([ref_res.atom(n).xyz() for n in chemical.rsd_to_fxnl_grp[ref_res.name()].atoms])
+    selector = pyrosetta.rosetta.core.select.residue_selector.ResidueIndexSelector()
+    pres = privileged_residues.PrivilegedResidues()
 
-        for p in pres.search(pose, ["sc_%s" % (suffix.lower())], selector):
-            res = p.residue(1)
-            coords = np.stack([res.atom(n).xyz() for n in chemical.functional_groups[res.name()].atoms])
+    selector.append_index(5)
 
-            RMSD = rmsd(ref_coords, coords)
-            if (RMSD < 0.25):
-                return
+    ref_res = pose.residue(2)
+    ref_coords = np.stack([ref_res.atom(n).xyz() for n in chemical.rsd_to_fxnl_grp[ref_res.name()].atoms])
 
-        assert(False)
+    for p in pres.search(pose, [bdtype], selector):
+        res = p.residue(1)
+        coords = np.stack([res.atom(n).xyz() for n in chemical.functional_groups[res.name()].atoms])
 
-    def test_sc_sc(self):
-        self.bidentate("Sc")
+        RMSD = rmsd(ref_coords, coords)
+        if (RMSD < 0.25):
+            return
 
-    def test_sc_scbb(self):
-        self.bidentate("ScBb")
-
-    def test_sc_bb(self):
-        self.bidentate("Bb")
+    assert(False)
 
